9536440b9aa74386a5fa9fbf7814ed1363ba24de
tiagoams <tiago.silva@cefas.co.uk>
2019-07-05 11:56:44 +0100

    Added time dependent salutations

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,13 @@ My first code.
 @author JGraham
 created 25/06/19
 '''
+import datetime
+current_time = datetime.datetime.now()
 
-hello = 'hello world'
-print(hello)
+
+if current_time.hour < 12:
+    print("Good morning")
+elif current_time.hour < 18:
+    print("Good afternoon")
+else:
+    print("Good evening")
