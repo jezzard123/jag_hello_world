49ca5e7aa91224377efef6b291e9242308744de4
Chyanna Allison (Cefas) <chyanna.allison@cefas.co.uk>
2021-06-15 14:48:16 +0100

    Adding little happy comment

--- hello_world.py
+++ hello_world.py
@@ -9,8 +9,10 @@ current_time = datetime.datetime.now()
 
 
 if current_time.hour < 12:
-    print("Good morning")
+    print("Good morning, Coffee Time")
+elif current_time.hour < 13:
+    print("Wow, Lunch")
 elif current_time.hour < 18:
     print("Good afternoon")
 else:
-    print("Good evening")
+    print("Good evening, enjoy your chill time")
